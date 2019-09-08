3544629cce48417c4f2496b74d220989598772b9
Adherwp <adheadhe71@gmail.com>
2019-09-08 10:24:38 +0700

    finish edit button

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
 	private EditText angkaInput;
+	private Button tebak;
 	int hasilRandom,min = 1,max = 100;
 
 	@Override
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
 		angkaInput = findViewById(R.id.number_input);
+		tebak = findViewById(R.id.guess_button);
 		this.initRandomNumber();
 	}
 
@@ -34,21 +36,29 @@ public class MainActivity extends AppCompatActivity {
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
 		String ubah= angkaInput.getText().toString();
-		int angka=Integer.parseInt(ubah);
 
-		if(angka==hasilRandom){
-			Toast.makeText(this,"Tebakan anda benar",Toast.LENGTH_SHORT).show();
-		}else if(angka>hasilRandom){
-			Toast.makeText(this,"Tebakan anda terlalu besar",Toast.LENGTH_SHORT).show();
-		}else{
-			Toast.makeText(this,"Teabakan anda terlalu kecil",Toast.LENGTH_SHORT).show();
+		if(ubah.isEmpty()){
+			Toast.makeText(this,"Isi terlebih dulu tebakan anda",Toast.LENGTH_SHORT).show();
+		}else {
+			int angka=Integer.parseInt(ubah);
+
+			if(angka>max || angka<min){
+				Toast.makeText(this,"Tebakan anda diluar range angka",Toast.LENGTH_SHORT).show();
+			}else if(angka==hasilRandom){
+				Toast.makeText(this,"Tebakan anda benar",Toast.LENGTH_SHORT).show();
+				tebak.setEnabled(false);
+			}else if(angka>hasilRandom){
+				Toast.makeText(this,"Tebakan anda terlalu besar",Toast.LENGTH_SHORT).show();
+			}else {
+				Toast.makeText(this, "Teabakan anda terlalu kecil", Toast.LENGTH_SHORT).show();
+			}
 		}
-
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
 		angkaInput.setText(null);
 		this.initRandomNumber();
+		tebak.setEnabled(true);
 	}
 }
